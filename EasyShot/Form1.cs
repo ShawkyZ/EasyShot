c85310f29bfa452be26cfbb1df5639eac5e737e7
Abdelrhman Shawky <abdelrhmanshawky4@gmail.com>
2015-01-19 18:29:04 +0200

    Some Initial Check

--- EasyShot/Form1.cs
+++ EasyShot/Form1.cs
@@ -8,7 +8,6 @@ using System.Drawing.Imaging;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EasyShot
